3b1d5264132478289bbd9a9783cec95f1463630b
Hesham Mohammad Tarique <63514840+Heshamtarique@users.noreply.github.com>
2023-03-03 09:54:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,12 @@ import streamlit as st
 
 
 
-#
-# def print_hi(name):
-#     print(f'Hi, {name}')
-#
-# def foo(x):
-#     print(x)
-
-
-# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 
 
 # side bar menu
 with st.sidebar:
-#     selected = option_menu(
-#         menu_title="Main Menu",
-#         options=["home", "Projects", "Work Experience", "Contact"])
 
-# if selected == "home":
-    st.title(f'Hi, I am Hesham, Welcome :wave:')
+    st.title(f'Hi, Initiating ABU project :wave:')
 
