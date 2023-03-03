5982d60a10aef640a5307e700ae3912ac963a858
Hesham Mohammad Tarique <63514840+Heshamtarique@users.noreply.github.com>
2023-03-03 09:53:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import spacy
 import streamlit as st
-from annotated_text import annotated_text
-import requests
-from streamlit_lottie import st_lottie
-from PIL import Image
-from streamlit_option_menu import option_menu
+
 
 
 
