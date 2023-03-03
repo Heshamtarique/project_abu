58e1439451882bfb54d77febc3b2a1ec5a977163
Hesham Mohammad Tarique <63514840+Heshamtarique@users.noreply.github.com>
2023-03-03 09:55:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,5 @@ st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 
 
-# side bar menu
-with st.sidebar:
-
-    st.title(f'Hi, Initiating ABU project :wave:')
+st.title(f'Hi, Initiating ABU project :wave:')
 
